7e64d458fcfbb6ca0632a359a2d3800d00bdac3d
Josh Bradshaw <joshbradshaw11@gmail.com>
2015-07-18 17:49:30 -0400

    added friction clutch

--- spring_system.py
+++ spring_system.py
@@ -3,25 +3,26 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 # parameters, set through measurement and making assumptions
-U1 = 5.
+U1 = 1.
 I2 = 1.
 C4 = 1.
-TF = 0.25
+TF = 1.
 I7 = 1.
-R8 = 1.1
-C9 = 1.
+R9 = 1.
+C11 = 1.
+R12 = 1.
 
 def f(y, t):
     P2i = y[0]
     Q4i = y[1]
     P7i = y[2]
-    Q9i = y[3]
+    Q11i = y[3]
     X2i = y[4]
 
     f0 = U1 - Q4i/C4
     f1 = P2i/I2 - P7i/(TF*I7)
-    f2 = TF*Q4i/C4 - P7i*R8 - Q9i/C9
-    f3 = P7i/I7
+    f2 = Q4i/(TF*C4) - R9 *(P7i/I7 + Q11i/(R12*C11))
+    f3 = P7i/I7 - Q11i/(R12*C11)
     f4 = P2i/I2
     return [f0, f1, f2, f3, f4]
 
@@ -29,9 +30,9 @@ def f(y, t):
 P2 = 0. # initial velocity
 Q4 = 0. # initial force
 P7 = 0. # initial force
-Q9 = 0. # initial velocity
+Q11 = 0. # initial velocity
 X2 = 0.
-y0 = [P2, Q4, P7, Q9, X2]
+y0 = [P2, Q4, P7, Q11, X2]
 t = np.linspace(0, 100, 2000)
 
 # solve the DEs
