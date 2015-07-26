9885c2486532e8e02601780c509d9578ccd61f17
Josh Bradshaw <joshbradshaw11@gmail.com>
2015-07-26 10:37:54 -0400

    working ok

--- spring_system.py
+++ spring_system.py
@@ -5,15 +5,15 @@ from scipy.integrate import odeint
 import math
 
 # parameters, set through measurement and making assumptions
-SE_1 = 2.94
-I_2 = 0.3
-C_4 = 136.0
-TF_5 = 4092.0
-I_7 = 0.00002405 # revolution
-R_9 = 0.001 # no slip for now
-R_11 = 1.0 # 
-TF_12 = 0.00024
-C_13 = 15.0
+SE_1 = 2.94 # N
+I_2 = 0.3 # kg
+C_4 = 1./136. # m/N
+TF_5 = 71 # rad/m
+I_7 = 0.002 # revolution
+R_9 = 1.6 # no slip for now
+R_11 = 1.6 # 
+TF_12 = 0.014 # m/rad
+C_13 = 1/15. # m/N
 
 # slip is given by X7 - X9
 
@@ -23,14 +23,16 @@ def f(y, t):
     Q4i = y[1]
     P7i = y[2]
     Q13i = y[3]
+    X1_ = y[4]
 
-    v11 = (1.0/(1.0 + R_9/R_11)) * ((R_9*P7i/(R_11*I_7)) - TF_12*Q13i/(R_11*C_13))
+    v11 = (R_9*P7i/I_7 - TF_12*Q13i/C_13)/(R_11+R_9)
     P2_ = SE_1 - Q4i/C_4
     Q4_ = P2i/I_2 - P7i/(TF_5 * I_7)
     P7_ = Q4i/(C_4*TF_5) - R_9*(P7i/I_7 - v11)
-    Q13_ = v11/TF_12
+    Q13_ = v11*TF_12
+    X1_ = P2_/I_2
 
-    return [P2_, Q4_, P7_, Q13_]
+    return [P2_, Q4_, P7_, Q13_, X1_]
 
 def u_k(vr, vt):
     """4th order model of the bushing friction"""
@@ -41,12 +43,13 @@ def u_k(vr, vt):
 
 
 # Initial conditions
-P2 = 0 # initial velocity
+P2 = 3. # initial velocity
 Q4 = 0. # initial force
 P7 = 0. # initial force
 Q13 = 0. # initial velocity
-y0 = [P2, Q4, P7, Q13]
-t = np.linspace(0, 1000, 2000)
+X1 = 0.
+y0 = [P2, Q4, P7, Q13, X1]
+t = np.linspace(0, 10, 4000)
 
 # solve the DEs
 soln = odeint(f, y0, t)
@@ -54,9 +57,10 @@ P2 = soln[:, 0]
 Q4 = soln[:, 1]
 P7 = soln[:, 2]
 Q13 = soln[:, 3]
+X1 = soln[:, 4]
 
 plt.figure()
-plt.plot(t, Q13, 'r--')
+plt.plot(t, X1, 'r--')
 
 plt.xlabel('Time in seconds')
 plt.ylabel('kg m/s')
